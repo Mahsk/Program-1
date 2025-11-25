c8dcea428fc35cfcbce6fd2865be0da24710c371
mahsk <maite_a_kamei@hotmail.com>
2025-11-25 15:42:57 -0300

    remove o repositorio git embutido

--- theboys_2/func_eventos.h
+++
@@ -1,9 +0,0 @@
-#ifndef  FUNC_EVENTOS
-#define FUNC_EVENTOS
-
-struct evento_t *cria_evento(int tempo, int tipo, int heroi, int base) ;
-
-struct evento_t *insere_lef(struct fprio_t *lef, int tempo, int tipo, int heroi, int base) ;
-
-
-#endif
